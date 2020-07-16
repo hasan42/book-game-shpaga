fc788bb40a1dc11364319b3f9fa874a7fec64521
rpokosov <hasan83842@gmail.com>
2020-07-16 22:43:15 +0300

    add new game, chane params
    Добавил скрипт создания новой игры, изменил некоторые параметры

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -28,16 +28,16 @@ class GameStore {
   playerStrength = 0; // текущая сила
   playerSpecial = null; // особая сила
   playerHonor = 0; // количество чести
-  playerGod = true; // обратиться к богу. Максимум 1 раз
+  playerGod = false; // обратиться к богу. Максимум 1 раз
   playerMoney = 0; // количество денег
   playerFood = 0; // количество еды. Максимально 2
   playerInventoryMax = 5; // максимальное количество предметов в инвентаре
   playerInventory = []; // инвентарь игрока
-  playerHorse = 1; // лошадь игрока
-  playerSword = 1; // меч игрока
+  playerHorse = 0; // лошадь игрока
+  playerSword = 0; // меч игрока
   playerDagger = 0; // кинжавл игрока
-  playerPistol = 1; // пистолет игрока
-  playerAmmo = 1; // патроны игрока
+  playerPistol = 0; // пистолет игрока
+  playerAmmo = 0; // патроны игрока
 
   constructor() {
     this.show = false;
@@ -61,56 +61,79 @@ class GameStore {
     switch (this.turnDice()) {
       case 1:
         this.playerAgilityMax = 12;
+        this.playerAgility = 12;
         break;
       case 2:
         this.playerAgilityMax = 8;
+        this.playerAgility = 8;
         break;
       case 3:
         this.playerAgilityMax = 10;
+        this.playerAgility = 10;
         break;
       case 4:
         this.playerAgilityMax = 7;
+        this.playerAgility = 7;
         break;
       case 5:
         this.playerAgilityMax = 9;
+        this.playerAgility = 9;
         break;
       default:
         this.playerAgilityMax = 11;
+        this.playerAgility = 11;
         break;
     }
+
     switch (this.turnDice()) {
       case 1:
         this.playerStrengthMax = 22;
+        this.playerStrength = 22;
         break;
       case 2:
         this.playerStrengthMax = 18;
+        this.playerStrength = 18;
         break;
       case 3:
         this.playerStrengthMax = 14;
+        this.playerStrength = 14;
         break;
       case 4:
         this.playerStrengthMax = 24;
+        this.playerStrength = 24;
         break;
       case 5:
         this.playerStrengthMax = 16;
+        this.playerStrength = 16;
         break;
       default:
         this.playerStrengthMax = 20;
+        this.playerStrength = 20;
         break;
     }
   }
 
-  // проверка удачи
+  // проверка удачи четно - повезло, нечетно - не повезло
   checkYouLuck() {
     return this.turnDice() % 2 === 0;
   }
 
-  // расчет урона
+  // расчет урона - равно = парирование, больше - урон врагу, меньше - урон игроку
   calculateFightHit(enemyAg) {
     const playerHit = this.turnDice() * 2 + this.playerAgility;
     const enemyHit = this.turnDice() * 2 + enemyAg;
-
     return playerHit === enemyHit ? 0 : playerHit > enemyHit ? 2 : -2;
+  }
+
+  // создание новой игры
+  createNewGame() {
+    this.calculatePlayerStat(); // проставляем статы
+    this.playerHonor = 3; // честь
+    this.playerHorse = 1; // лошадь
+    this.playerSword = 1; // меч
+    this.playerPistol = 1; // пистолет
+    this.playerMoney = 15; // деньги
+    this.playerGod = true; // обращение к богу
   }
 
   toggleIntro() {
@@ -127,6 +150,18 @@ class GameStore {
   }
 }
 decorate(GameStore, {
+  playerAgility: observable,
+  playerStrength: observable,
+  playerHonor: observable,
+  playerGod: observable,
+  playerMoney: observable,
+  playerFood: observable,
+  playerInventory: observable,
+  playerHorse: observable,
+  playerSword: observable,
+  playerDagger: observable,
+  playerPistol: observable,
+  playerAmmo: observable,
   show: observable,
   introText: observable,
   isShowIntro: computed,
