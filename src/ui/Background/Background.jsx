764a1099a43d5c96e320697a29cf06554a3d5d64
rpokosov <hasan83842@gmail.com>
2020-10-04 15:03:13 +0300

    del useless
    Не много подчистил варнинги - удалил лишнее в компонентах.

--- src/ui/Background/Background.jsx
+++ src/ui/Background/Background.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { observer, inject } from "mobx-react";
-import gameStore from "@stores/gameStore";
 import "./Background.css";
 
-const Background = inject("gameStore")(
-  observer(({ GameStore, children, image }) => {
-    const bgImage = process.env.PUBLIC_URL + image;
+const Background = (children, image) => {
+  const bgImage = process.env.PUBLIC_URL + image;
 
-    return (
-      <div className="background">
-        <div
-          className="background__image"
-          style={{ backgroundImage: `url(${bgImage})` }}
-        ></div>
-        <div className="background__content">{children}</div>
-      </div>
-    );
-  })
-);
+  return (
+    <div className="background">
+      <div
+        className="background__image"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      ></div>
+      <div className="background__content">{children}</div>
+    </div>
+  );
+};
 
 export default Background;
--- src/ui/EnemyImage/EnemyImage.jsx
+++ src/ui/EnemyImage/EnemyImage.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import { observer, inject } from "mobx-react";
 import HealthBar from "../HealthBar/HealthBar";
-import gameStore from "@stores/gameStore";
 import "./EnemyImage.css";
 
-const EnemyImage = inject("gameStore")(
-  observer(({ GameStore, image, name, health }) => {
-    const bgImage = process.env.PUBLIC_URL + image;
+const EnemyImage = (image, name, health) => {
+  const bgImage = process.env.PUBLIC_URL + image;
 
-    return (
-      <div className="enemy">
-        <div className="enemy__name">{name}</div>
-        <HealthBar health={health} />
-        <div
-          className="enemy__image"
-          style={{ backgroundImage: `url(${bgImage})` }}
-        >
-          <img src={bgImage} alt={name} />
-        </div>
+  return (
+    <div className="enemy">
+      <div className="enemy__name">{name}</div>
+      <HealthBar health={health} />
+      <div
+        className="enemy__image"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
+        <img src={bgImage} alt={name} />
       </div>
-    );
-  })
-);
+    </div>
+  );
+};
 
 export default EnemyImage;
