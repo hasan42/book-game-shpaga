d1f2458f040ece99427a42d6f6ab38e224166415
rpokosov <hasan83842@gmail.com>
2020-12-03 21:39:33 +0300

    #2 try make animation
    Тестовая проба сделать анимацию

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  Redirect,
+  withRouter,
+  useLocation,
+} from "react-router-dom";
+// import { useTransition, animated } from "react-spring";
 
 import MainNavigation from "./ui/MainNavigation/MainNavigation";
 import Layout from "./ui/Layout/Layout";
@@ -32,3 +39,29 @@ class App extends Component {
 }
 
 export default withRouter(App);
+
+// export default function App() {
+//   const location = useLocation();
+//   const transitions = useTransition(location, (location) => location.pathname, {
+//     from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+//     enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+//     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+//   });
+//   return (
+//     <Layout>
+//       <MainNavigation></MainNavigation>
+//       {transitions.map(({ item: location, props, key }) => (
+//         <animated.div key={key} style={props}>
+//           <Switch location={location}>
+//             <Route path="/road-map" component={RoadMap} />
+//             <Route path="/game/:id" component={GamePage} />
+//             <Route path="/character" component={CreateCharacter} />
+//             <Route path="/intro" component={IntroPage} />
+//             <Route path="/" exact component={HomePage} />
+//             <Redirect to="/" />
+//           </Switch>
+//         </animated.div>
+//       ))}
+//     </Layout>
+//   );
+// }
